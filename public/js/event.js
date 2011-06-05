b1776025e4a7f5f5b685748c91f00a247de9dfff
umjasnik@github.com <mail@uwefleischer.de>
2011-06-05 12:47:42 +0200

    added map to view events

--- public/js/event.js
+++ public/js/event.js
@@ -1,12 +1,39 @@
+var animation, animationIndex = 0,
+items = $(".stream-item"),
+
+// map
+var map = new OpenLayers.Map("map", { controls: []}),
+osm = new OpenLayers.Layer.OSM("OSM"),
+places = null,
+layer = new OpenLayers.Layer.Vector();
+map.addLayer(osm);	
+map.setBaseLayer(osm);
+map.zoomTo(1);
+map.addLayer(layer);
+new OpenLayers.Request.GET({
+	url: "places.json",
+	success: function(response) {
+		places = JSON.parse(response.responseText);
+		startAnimation();
+	}
+});
+
+// select element
 var selectedElement = null, offsetTop = 0;
 var showItem = function(ele) {
 	ele.removeClass('night');
-/*
-	var place = $('.place');
-	if (0 < place.lenght) {
-		console.log(place);
+	var place = ele.find('.place').text().trim();
+	if ('' != place) {
+		place = places[place];
+		layer.removeAllFeatures();
+		var point = new OpenLayers.LonLat(place.latitude, place.longitude).transform(
+			new OpenLayers.Projection("EPSG:4326"),
+			map.getProjectionObject()
+		);
+		point = new OpenLayers.Geometry.Point(point.lon, point.lat);
+		feature = new OpenLayers.Feature.Vector(point);
+		layer.addFeatures([feature]);
 	}
-*/
 	$("#infoboard .info-item").html(ele);
 }
 $(".stream-item").click(function() {
@@ -14,19 +41,20 @@ $(".stream-item").click(function() {
 		selectedElement.removeClass('selected');
 	}
 	selectedElement = $(this);
-	showItem($(this).clone());
+	showItem(selectedElement.clone());
 	selectedElement.addClass('selected');
 	offsetTop = selectedElement[0].offsetTop - 300;
 	window.scrollTo(0, offsetTop);
 });
 
+
 // animation
-var items = $(".stream-item"),
-i = 0,
-animation = setInterval(function() {
-	$(items[i]).trigger('click');
-	if (!items[i + 1]) {
-		clearInterval(animation);
-	}
-	i++;
-}, 100);
+var startAnimation = function() {
+	animation = setInterval(function() {
+		$(items[animationIndex]).trigger('click');
+		if (!items[animationIndex + 1]) {
+			clearInterval(animation);
+		}
+		animationIndex++;
+	}, 100);
+}
